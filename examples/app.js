e02dae39f46c42ef87fdaeb04952dd948a1fb698
Dhi Aurrahman <dio@hooq.tv>
2016-05-28 05:46:46 +0700

    More async await in example

--- examples/app.js
+++ examples/app.js
@@ -1,6 +1,14 @@
+/* eslint-disable one-var */
 import { boot, send } from '../lib';
 
+const delay = () => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, 1000);
+    });
+};
+
 const fn = async (req, res) => {
+    await delay();
     send(res, 200, { OK: '1' });
 };
 
